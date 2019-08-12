f34ebdba968bafb36775f7632d684bf42e265167
Matheus <matheus.jesuino2@gmail.com>
2019-08-12 17:13:29 -0300

    App com redux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import CourseList from './components/CourseList'
+
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <CourseList />
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/CourseList/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function addCourseAction(title) {
+	return { type: 'ADD_COURSE', title };
+}
+
+export default function CourseList() {
+	const qty = 5;
+
+	const courses = useSelector(
+		state => state.data.slice(0, qty),
+		[qty]
+	);
+	const dispatch = useDispatch()
+
+	function addCourse(){ 
+		dispatch(addCourseAction('JS'))
+	}
+
+	return (
+		<>
+			<ul>
+				{ courses.map(course => <li key={course}>{course}</li>) }
+			</ul>
+			<button type="button" onClick={addCourse}>
+				Adicionar Curso
+			</button>
+		</>
+	);
+};
